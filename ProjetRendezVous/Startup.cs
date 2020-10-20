0fbe0ca4534b84924f3edb5404041103446e4612
Yassir achamrar <yassir.achamrar@gmail.com>
2020-10-20 10:50:00 +0100

    First Update

---
+++ ProjetRendezVous/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetRendezVous.Startup))]
+namespace ProjetRendezVous
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

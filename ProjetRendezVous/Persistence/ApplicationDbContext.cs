21fff32fa59dc593d067fffc0827039ccf08b29a
ANAS10-kek <60491830+ANAS10-kek@users.noreply.github.com>
2020-10-20 17:33:03 +0100

    dashboard static

--- ProjetRendezVous/Persistence/ApplicationDbContext.cs
+++ ProjetRendezVous/Persistence/ApplicationDbContext.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using ProjetRendezVous.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace ProjetRendezVous.Persistence
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public System.Data.Entity.DbSet<ProjetRendezVous.Models.ApplicationUser> ApplicationUsers { get; set; }
     }
 }
